503f66ecbf6b608c5c9977412720d8f9d83bcf72
edlee <locutus709@yahoo.com>
2010-11-26 03:14:12 -0500

    Changes through Chap 4

--- Gemfile
+++ Gemfile
@@ -1,13 +1,14 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.0.rc2'
+gem 'rails', '3.0.3'
 
 gem 'sqlite3-ruby', '1.2.5', :require => 'sqlite3'
 
 group :development do
-  gem 'rspec-rails', '2.0.0.beta.18'
+  gem 'rspec-rails', '2.1.0'
 end
 
 group :test do
-   gem 'rspec', '2.0.0.beta.18'
+   gem 'rspec', '2.1.0'
+   gem 'webrat', '0.7.1'
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  
+  #Return a title on a per page basis
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+  
+  def junktext
+    "This is a junk string"
+  end
+  
+  def junktext2
+    "Line 1 More junk...."
+    "Line 2 More junk...."
+    "Line 3 More junk...."
+    2+3
+    2 == 2
+  end
+  
 end
